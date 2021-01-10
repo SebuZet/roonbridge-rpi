aae133e66d8f66edc01e433282a584e046473029
SebuZet <39659949+SebuZet@users.noreply.github.com>
2021-01-11 00:16:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-wget ${HIFIBERRY_BASE_URL}/${CONFIGURE_RAAT}
-wget ${HIFIBERRY_BASE_URL}/${RAAT_APP}
-chmod +x ${CONFIGURE_RAAT} && \
-chmod +x ${RAAT_APP} && \
-[ -s /raat/uuid ] || /usr/bin/uuidgen > /raat/uuid
-./${CONFIGURE_RAAT}
+# wget ${HIFIBERRY_BASE_URL}/${CONFIGURE_RAAT}
+#wget ${HIFIBERRY_BASE_URL}/${RAAT_APP}
+#chmod +x ${CONFIGURE_RAAT} && \
+#chmod +x ${RAAT_APP} && \
+#[ -s /raat/uuid ] || /usr/bin/uuidgen > /raat/uuid
+#./${CONFIGURE_RAAT}
 
-exec /raat/${RAAT_APP} /raat/hifiberry_raat.conf
+exec /opt/RoonBridge/start.sh
+#/raat/${RAAT_APP} /raat/hifiberry_raat.conf
